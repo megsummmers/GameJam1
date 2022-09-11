0eb331b6364df595a743939d6b2549f545ecc6cd
megsummmers <70597858+megsummmers@users.noreply.github.com>
2022-09-11 11:55:16 -0400

    Update: finished movement
    - player moves to and from select locations
    - easy to add/ change locations
    - player sprite follows the mouse (not perfected yet)

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
   public GameObject desk;
   public GameObject boss;
   public GameObject water;
+  public GameObject printer;
+  public GameObject bathroom;
+  public GameObject random;
   private float speed = 1.5f;
   private float place_num;
 
@@ -27,14 +29,36 @@ public class Movement : MonoBehaviour
       //Rotates the player to face the mouse
       transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(mouse.y, mouse.x) * Mathf.Rad2Deg - 90);
       //consults random number to see where the player is moving too
-      if(place_num == 1){
-        //Moves the player towards the place of interest
+      switch(place_num)
+      {
+        case 1: //desk
         transform.position = Vector2.MoveTowards(transform.position, desk.transform.position, speed * Time.deltaTime);
-      } else if (place_num == 2){
+        break;
+        case 2: //boss
         transform.position = Vector2.MoveTowards(transform.position, boss.transform.position, speed * Time.deltaTime);
-      } else if (place_num == 3){
+        break;
+        case 3: //water
         transform.position = Vector2.MoveTowards(transform.position, water.transform.position, speed * Time.deltaTime);
+        break;
+        case 4: //printer
+        transform.position = Vector2.MoveTowards(transform.position, printer.transform.position, speed * Time.deltaTime);
+        break;
+        case 5: //bathroom
+        transform.position = Vector2.MoveTowards(transform.position, bathroom.transform.position, speed * Time.deltaTime);
+        break;
+        case 6: //random place
+        transform.position = Vector2.MoveTowards(transform.position, random.transform.position, speed * Time.deltaTime);
+        break;
       }
+    }
 
+    public void OnTriggerEnter2D(Collider2D collision){
+      float currentNum = place_num;
+      if(collision.gameObject.tag == "place"){
+        place_num = Random.Range(1, 7);
+        while(place_num == currentNum){
+          place_num = Random.Range(1, 7);
+        }
+      }
     }
 }
