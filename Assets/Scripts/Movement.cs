27a68ac4b274009b10447c57499856f319addfe8
megsummmers <70597858+megsummmers@users.noreply.github.com>
2022-09-14 15:45:21 -0400

    Working on clock

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Movement : MonoBehaviour
 {
@@ -10,8 +11,12 @@ public class Movement : MonoBehaviour
   public GameObject printer;
   public GameObject bathroom;
   public GameObject random;
+  public TextMeshProUGUI clock;
+  public GameObject gameOverTxt;
   private float speed = 1.2f;
   private float place_num;
+  private float timeLeft = 30;
+  private bool gameOn = true;
 
     // Start is called before the first frame update
     void Start()
@@ -23,32 +28,41 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      //subtracts one every second
+      timeLeft -= Time.deltaTime;
+      //change clock text
+      clock.SetText($"{timeLeft}");
+      if ( timeLeft < 0 ){
+        GameOver();
+      }
       //get mouse Position
       Vector3 mousePos = Input.mousePosition;
       Vector3 mouse = Camera.main.ScreenToWorldPoint(mousePos);
-      //Rotates the player to face the mouse
-      transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(mouse.y, mouse.x) * Mathf.Rad2Deg - 90);
-      //consults random number to see where the player is moving too
-      switch(place_num)
-      {
-        case 1: //desk
-        transform.position = Vector2.MoveTowards(transform.position, desk.transform.position, speed * Time.deltaTime);
-        break;
-        case 2: //boss
-        transform.position = Vector2.MoveTowards(transform.position, boss.transform.position, speed * Time.deltaTime);
-        break;
-        case 3: //water
-        transform.position = Vector2.MoveTowards(transform.position, water.transform.position, speed * Time.deltaTime);
-        break;
-        case 4: //printer
-        transform.position = Vector2.MoveTowards(transform.position, printer.transform.position, speed * Time.deltaTime);
-        break;
-        case 5: //bathroom
-        transform.position = Vector2.MoveTowards(transform.position, bathroom.transform.position, speed * Time.deltaTime);
-        break;
-        case 6: //random place
-        transform.position = Vector2.MoveTowards(transform.position, random.transform.position, speed * Time.deltaTime);
-        break;
+      if(gameOn){
+        //Rotates the player to face the mouse
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(mouse.y, mouse.x) * Mathf.Rad2Deg - 90);
+        //consults random number to see where the player is moving too
+        switch(place_num)
+        {
+          case 1: //desk
+          transform.position = Vector2.MoveTowards(transform.position, desk.transform.position, speed * Time.deltaTime);
+          break;
+          case 2: //boss
+          transform.position = Vector2.MoveTowards(transform.position, boss.transform.position, speed * Time.deltaTime);
+          break;
+          case 3: //water
+          transform.position = Vector2.MoveTowards(transform.position, water.transform.position, speed * Time.deltaTime);
+          break;
+          case 4: //printer
+          transform.position = Vector2.MoveTowards(transform.position, printer.transform.position, speed * Time.deltaTime);
+          break;
+          case 5: //bathroom
+          transform.position = Vector2.MoveTowards(transform.position, bathroom.transform.position, speed * Time.deltaTime);
+          break;
+          case 6: //random place
+          transform.position = Vector2.MoveTowards(transform.position, random.transform.position, speed * Time.deltaTime);
+          break;
+        }
       }
     }
 
@@ -65,5 +79,11 @@ public class Movement : MonoBehaviour
         //hide the food item
         collision.gameObject.SetActive(false);
       }
+    }
+
+    public void GameOver(){
+      gameOn = false;
+      transform.position = new Vector3(0, 0, 0);
+
     }
 }
